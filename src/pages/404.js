3c6c063f91abdaab139ff8721a6027b13eb16aaf
soggyinkgames <soggyinkgames@gmail.com>
2021-11-11 00:50:02 +1100

    add 404 page with SIG gif

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,6 +14,7 @@ class NotFoundPage extends React.Component {
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <img src="../../content/assets/Soggy404.gif" alt="broken tv soggy ink logo gif"></img>
       </Layout>
     )
   }
